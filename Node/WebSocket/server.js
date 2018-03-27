2bf0993798807b329912b2419eb1d19f7ca9d946
yaokunpeng <y1050661817@gmail.com>
2018-03-27 17:48:46 +0800

    ADD:websocket通讯实例,借用socket.io库

---
+++ Node/WebSocket/server.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const http = require('http');
+
+let server = http.createServer((req,res)=>{
+    if (req.url == '/') {
+        //显示首页
+        fs.readFile('./we-pain.html',(err,data)=>{
+            res.end(data);
+        });
+    }
+});
+
+
+//创建一个socketio对象
+const io = require('socket.io')(server);
+
+io.on("connection",(socket)=>{
+    console.log('someone connected');
+    //对每个连接 的 客户端用户监听paint事件
+    socket.on("paint",(paintInfo)=>{
+        //广播 获得的数据到所有客户端
+
+        io.emit("otherpaint",paintInfo)
+    });
+});
+
+
+server.listen(8090,"127.0.0.1");
