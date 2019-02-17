a80b52183f147ee5d253a4ec5cf342fba43317d7
yaokunpeng <y1050661817@gmail.com>
2019-02-17 12:28:09 +0800

    ADD：base transition component learn from airen

---
+++ FrontEnd-SkillPractice/Vue-Practice/Widget-Vue-Project/src/components/base-transition/fadeTransition.vue
@@ -0,0 +1,46 @@
+<template>
+  <component :is="componentType"
+             :tag="tag"
+             v-bind="$attrs"
+             v-on="hooks"
+             enter-active-class="fadeIn"
+             move-class="fade-move"
+             leave-active-class="fadeOut">
+    <slot></slot>
+  </component>
+</template>
+<script>
+  import baseTransition from '../mixins/baseTransition.js'
+  export default {
+    name: 'fade-transition',
+    mixins: [baseTransition]
+  }
+</script>
+<style>
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  .fadeIn {
+    animation-name: fadeIn;
+  }
+  @keyframes fadeOut {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+  .fadeOut {
+    animation-name: fadeOut;
+  }
+  /* 手动添加了一个move类，指定transform持续的时间 */
+  .fade-move {
+    transition: transform .3s ease-out;
+  }
+</style>
