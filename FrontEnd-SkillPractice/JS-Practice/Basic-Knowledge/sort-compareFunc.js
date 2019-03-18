9675b3f8f8dbedd7b97596ebee0a1b66942451db
yaokunpeng <y1050661817@gmail.com>
2019-03-18 21:57:31 +0800

    [ADD]： (tool function)array sort compare

---
+++ FrontEnd-SkillPractice/JS-Practice/Basic-Knowledge/sort-compareFunc.js
@@ -0,0 +1,34 @@
+/**
+ * @description compare函数
+ * @param {Object} params {key,order}
+ * @param {String} key 排序键
+ * @param {String} order 排序方式，升序 降序[Asc|Des]
+ */
+
+// let compareFunc = order === 'Asc' ? new Function('x', 'y', 'return ((x < y) ? -1 : (x > y) ? 1 : 0)') : new Function('x', 'y', 'return ((x > y) ? -1 : (x < y) ? 1 : 0)')
+// let compareStrategy = {
+//     'Des': (x, y) => ((x > y) ? -1 : (x < y) ? 1 : 0),
+//     'Asc': (x, y) => ((x < y) ? -1 : (x > y) ? 1 : 0)
+// }
+// let compareFunc = compareStrategy[order]
+
+function compareByFunc(options = {}) {
+    let key = options.key || ''
+    let order = options.order || 'Asc'
+    let compareFunc = order === 'Asc' ? (x, y) => ((x < y) ? -1 : (x > y) ? 1 : 0) : (x, y) => ((x > y) ? -1 : (x < y) ? 1 : 0)
+    let get = key ? x => x[key] : x => x
+    return function (a, b) {
+        var x = get(a)
+        var y = get(b)
+        return compareFunc(x, y)
+    }
+}
+
+let a = [6, 89, 33, 221, 1]
+let b = [{ 'a': 6 }, { 'a': 89 }, { 'a': 33 }, { 'a': 221 }, { 'a': 1 }]
+
+a.sort(compareByFunc())
+b.sort(compareByFunc({ key: 'a' }))
+console.log(compareByFunc().toString())
+console.log(a)
+console.log(b)
