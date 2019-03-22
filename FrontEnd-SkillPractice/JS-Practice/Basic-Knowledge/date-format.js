601b62bbe51c812f176481b1bfefd0876b3b6416
yaokunpeng <y1050661817@gmail.com>
2019-03-22 22:40:21 +0800

    [ADD]：display date type based on different time stamp

--- FrontEnd-SkillPractice/JS-Practice/Basic-Knowledge/date-format.js
+++ FrontEnd-SkillPractice/JS-Practice/Basic-Knowledge/date-format.js
@@ -15,3 +15,38 @@ function formatDate(type, time = new Date()) {
         .replace('ss', ('00' + date.getSeconds()).slice(-2))
         .replace('xxx', ('000' + date.getMilliseconds()).slice(-3))
 }
+
+
+
+/**
+ * @description 时间显示模式策略
+ * @param {Int} time 毫秒时间戳   '--:--'为默认显示时间格式
+ * @param {*} innerFormat 策略时间戳之内-时间格式
+ * @param {*} outerFormat 超过策略时间戳之外-时间格式
+ * @return {String} 应用当前策略后的时间格式
+ */
+// 以当天零点时间戳为分界
+function zeroTimeStampFunc(time, innerFormat, outerFormat) {
+    function zeroTimeStamp() {
+        let zeroTimeStamp = new Date().setHours(0, 0, 0, 0)
+        return time >= zeroTimeStamp
+    }
+    return timeJudge(time, innerFormat, outerFormat, zeroTimeStamp())
+}
+// 以当前时间24小时前为分界
+function twentyFourHoursFunc(time, innerFormat, outerFormat) {
+    function twentyFourHours() {
+        let twentyFourHoursTimeStamp = 24 * 60 * 60 * 1000
+        return (new Date().getTime() - time) > twentyFourHoursTimeStamp
+    }
+    return timeJudge(time, innerFormat, outerFormat, twentyFourHours())
+}
+function timeJudge(time, innerFormat, outerFormat, Strategy) {
+    if (time === '--:--') { return time }
+    return Strategy ? formatDate(innerFormat, time * 1) : formatDate(outerFormat, time * 1)
+}
+let DisplayTimeStrategy = {
+    'zeroTimeStamp': zeroTimeStampFunc,
+    'twentyFourHours': twentyFourHoursFunc
+}
+export const formatDisplayTime = DisplayTimeStrategy['zeroTimeStamp']
