3662a709b667966208393106c3aad51ad5aa7f9f
yaokunpeng <y1050661817@gmail.com>
2019-04-06 21:02:09 +0800

    [ADD]：(FP) value

---
+++ FrontEnd-SkillPractice/FP-Glossary/Value.js
@@ -0,0 +1,8 @@
+// Value(值)
+// Anything that can be assigned to a variable.（任何值都可分配给变量）
+
+let variable1 = 5
+const immutability = Object.freeze({name:'john',age:30})
+let variable2 = (a)=>a
+let variable3 = [1]
+let variable4 = undefined
