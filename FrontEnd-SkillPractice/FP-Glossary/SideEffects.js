02e8723b152b65f1616769919e16b40b3db545fe
yaokunpeng <y1050661817@gmail.com>
2019-04-21 18:28:50 +0800

    ADD：(FP) Side effects

---
+++ FrontEnd-SkillPractice/FP-Glossary/SideEffects.js
@@ -0,0 +1,4 @@
+// Side effects(副作用)
+// A function or expression is said to have a side effect if apart from returning a value,
+// it interacts with (reads from or writes to) external mutable state.
+// 如果一个函数或表达式除了返回一个值之外，还与外部可变状态进行交互（读或写），则称它具有副作用。
