0bd0d76e218b15390319417e2d36d1ae78473dbe
yaokunpeng <y1050661817@gmail.com>
2019-07-01 22:59:16 +0800

    ADD：(proxy) observable in proxy

---
+++ FrontEnd-SkillPractice/JS-Practice/ES6/proxy/observe.js
@@ -0,0 +1,23 @@
+// 观察者模式
+
+function set(target, key, value, receiver) {
+  const result = Reflect.set(target, key, value, receiver);
+  queuedObservers.forEach(observer => observer());
+  return result;
+}
+const queuedObservers = new Set();
+const observe = fn => queuedObservers.add(fn);
+const observable = obj => new Proxy(obj, { set });
+
+
+const person = observable({
+  name: '张三',
+  age: 20
+});
+
+function print() {
+  console.log(`${person.name}, ${person.age}`)
+}
+
+observe(print);
+person.name = '李四';
