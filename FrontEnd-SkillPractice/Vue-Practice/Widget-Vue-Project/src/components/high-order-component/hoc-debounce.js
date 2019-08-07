e2ce0f3a2573bc2ad54cc0fc4de21e8531702064
yaokunpeng <y1050661817@gmail.com>
2019-08-07 22:30:33 +0800

    Doc：(Vue) what is abstract component in vue

--- FrontEnd-SkillPractice/Vue-Practice/Widget-Vue-Project/src/components/high-order-component/hoc-debounce.js
+++ FrontEnd-SkillPractice/Vue-Practice/Widget-Vue-Project/src/components/high-order-component/hoc-debounce.js
@@ -1,13 +1,16 @@
 // 看过 Vue 源码的同学可以知道，<keep-alive>、<transition>、<transition-group>等组件
 // 组件的实现是一个对象，注意它有一个属性 abstract 为 true，表明是它一个抽象组件。
 
+// 抽象组件与普通组件类似，只是它们不向DOM呈现任何内容。他们只是为现有的行为添加额外的行为。
+// 你可能熟悉Vue内置那些抽象组件，如<transition>，<component>和<slot>。
+
 // Vue 的文档没有提这个概念，在抽象组件的生命周期过程中，我们可以对包裹的子组件监听的事件进行拦截，
 // 也可以对子组件进行 Dom 操作，从而可以对我们需要的功能进行封装，而不需要关心子组件的具体实现。
 import { get, debounce, set } from 'loadsh';
 
 export default {
     name: 'debounce',
-    abstract: true, //标记为抽象组件
+    abstract: true, //标记为抽象组件,没有此标记不影响组件正常运行
     props: {
         events: String,
         wait: {
