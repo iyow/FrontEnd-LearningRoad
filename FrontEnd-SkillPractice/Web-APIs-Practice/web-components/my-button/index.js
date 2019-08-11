f1a8ee27c037a33e60abd6b10e419ec88d0a001e
yaokunpeng <y1050661817@gmail.com>
2019-08-11 20:27:54 +0800

    ADD：(web components) custom button

---
+++ FrontEnd-SkillPractice/Web-APIs-Practice/web-components/my-button/index.js
@@ -0,0 +1,70 @@
+import template from './tpl-my-button.js'
+class Button extends HTMLElement {
+    constructor() {
+        super();
+
+        this._shadowRoot = this.attachShadow({ mode: 'open' });
+        this._shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.$container = this._shadowRoot.querySelector('.container');
+        this.$button = this._shadowRoot.querySelector('button');
+
+        // 如何将函数传递给Web组件进行交互？
+        // 1.使用属性传递，但是传过来的是字符串
+        // 2.内置事件交互
+        // 3.使用自定义事件，外部在该dom节点下监听自定义事件
+        this.$button.addEventListener('click', () => {
+            this.dispatchEvent(
+                new CustomEvent('onCustomClick', {
+                    detail: 'Hello from within the Custom Element',
+                })
+            );
+        });
+    }
+
+    // 拦截gettter 直接获取属性值
+    get label() {
+        console.log('label------getter触发');
+
+        return this.getAttribute('label');
+    }
+
+    set label(value) {
+        console.log('label------setter触发');
+
+        // 通过js修改 property 无法触发 dom 的 attribute 改变
+        // 所以拦截setter 手动设置dom的attribute
+        // 进而触发生命周期attributeChangedCallback
+        this.setAttribute('label', value);
+    }
+
+    // 生命周期
+    // 使用attributeChangedCallback 以及 js属性(property)和dom属性(attribute)的getter setter实现响应
+    static get observedAttributes() {
+        console.log('生命周期属性监听------get触发');
+
+        return ['label'];
+    }
+
+    attributeChangedCallback(name, oldVal, newVal) {
+        console.log('生命周期属性监听------回调函数触发', name, oldVal, newVal);
+
+        this.render();
+    }
+    connectedCallback() {
+        // 原子设计原则
+        if (this.hasAttribute('as-atom')) {
+            this.updateAsAtom();
+        }
+    }
+    updateAsAtom() {
+        this.$container.style.padding = '0px';
+    }
+
+    render() {
+        // 触发 label 的getter获取 值
+        this.$button.innerHTML = this.label;
+    }
+}
+
+window.customElements.define('my-button', Button);
---
+++ FrontEnd-SkillPractice/Web-APIs-Practice/web-components/my-button/tpl-my-button.js
@@ -0,0 +1,38 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+  <style>
+    .container {
+      padding: 8px;
+    }
+
+    button {
+      display: block;
+      overflow: hidden;
+      position: relative;
+      padding: 0 16px;
+      font-size: 16px;
+      font-weight: bold;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      cursor: pointer;
+      outline: none;
+
+      width: 100%;
+      height: 40px;
+
+      box-sizing: border-box;
+      border: 1px solid #a1a1a1;
+      background: #ffffff;
+      box-shadow: 0 2px 4px 0 rgba(0,0,0, 0.05), 0 2px 8px 0 rgba(161,161,161, 0.4);
+      color: #363636;
+      cursor: pointer;
+    }
+  </style>
+
+  <div class="container">
+    <button>Label</button>
+  </div>
+`;
+
+export default template
