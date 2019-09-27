dad4d856379ca4b99b2f23408f0a58da63828098
yaokunpeng <y1050661817@gmail.com>
2019-09-27 21:35:54 +0800

    ADD：(python) [data type] type hint and type check

--- Python/01_基本数据类型/08_type.py
+++ Python/01_基本数据类型/08_type.py
@@ -1,9 +1,17 @@
+import typing
+
+# 生成复合类型, 类型别名
+FloatVector = typing.List[float]
+StrList = typing.List[str]
+MyT = typing.TypeVar('T')
+
+
 class Role:
     pass
 
 
 r = Role()
-# isinstance(r, Role)
+
 print(Role)
 # 查看变量r的类型
 print(type(r))
@@ -20,7 +28,22 @@ print(type(Role))
 
 
 class X(object):
-    a = 1
+    def __init__(self):
+        # type: () -> None
+        self.num = 4  # type: int
+        self.color = '蓝色'  # type: str
+        self.elements = []  # type: List[int]
+
+    def add(self, param1: str) -> str:
+        # type: (str) -> str
+        """第二种需求
+        比较简单, 需要先了解 typing 这个库
+        :param a:
+        :param b:
+        :param c:
+        :return:
+        """
+        pass
 
 
 XX = type('XX', (object,), dict(a=1))  # 产生一个新的类型 XX
@@ -35,3 +58,12 @@ print(type(XX))
 print('类型------实例化X和XX')
 print(type(X()))
 print(type(XX()))
+
+
+# type hint
+def type_func(param: str) -> str:
+    pass
+
+
+print(typing.get_type_hints(X))
+print(typing.get_type_hints(type_func))
