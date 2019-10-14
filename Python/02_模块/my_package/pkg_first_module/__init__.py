1369bf6c2e147a9debd203bedfe24ca43197ac19
yaokunpeng <y1050661817@gmail.com>
2019-10-14 21:36:46 +0800

    Doc：(python) add some knowledge points

--- Python/02_模块/my_package/pkg_first_module/__init__.py
+++ Python/02_模块/my_package/pkg_first_module/__init__.py
@@ -3,6 +3,9 @@
 # 导入时 使用 from my_package from * 时
 __all__ = ["module1", "module2"]
 
+# __init__.py 文件是通过如下 2 种方式来导入包中模块的：
+#  从当前包导入指定模块 ,from . import 模块名 
+#  从.模块名 导入所有成员到包中 ,from .模块名 import *
 # 外部导入时 使用 import my_package 时
 from . import module1
 from . import module2
