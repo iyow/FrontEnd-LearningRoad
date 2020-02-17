263e15fb76b76c6129af3023b37ab8b6e9856959
yaokunpeng <y1050661817@gmail.com>
2020-02-17 21:33:13 +0800

    FIX：(vue) wrap hoc

--- FrontEnd-SkillPractice/Vue-Practice/Widget-Vue-Project/src/components/high-order-component/hoc-debounce-m2.js
+++ FrontEnd-SkillPractice/Vue-Practice/Widget-Vue-Project/src/components/high-order-component/hoc-debounce-m2.js
@@ -1,51 +1,99 @@
 // 直接 把按钮组件 封装 生成 新的组件形式
 // 可使用该方式 全局复写按钮组件 实现一键热替换(类似monkey patch概念？)
 import { debounce } from 'loadsh';
-// 导出新组件
-export default {
-    name: 'DebounceButton',
-    props: {
-        wait: {
-            type: Number,
-            default: 300
-        },
-        options: {
-            type: Object,
-            default() {
-                return {};
-            }
-        }
-    },
+
+
+function hoc(WrappedComponent, options) {
+  return {
+    inheritAttrs: false,
     mounted() {
-        console.log('HOC succeed')
-    },
-    methods: {
-        handleClickLink(event) {
-            console.log('debounce')
-            // this.$listeners.click为绑定在该组件上的click函数
-            debounce(this.$listeners.click, this.wait, this.options)
-        }
+      console.log('I have already mounted')
     },
     render(h) {
-        const slots = Object.keys(this.$slots)
-            .reduce((arr, key) => arr.concat(this.$slots[key]), [])
-            .map(vnode => {
-                vnode.context = this._self
-                return vnode
-            })
-        //eg. el-button
-        return h('Button', {
-            on: {
-                click: this.handleClickLink //新组件绑定click事件
-            },
-            props: this.$props,
-            // 透传 scopedSlots
-            scopedSlots: this.$scopedSlots,
-            attrs: this.$attrs
-        }, slots)
+      const slots = Object.keys(this.$slots)
+        .reduce((arr, key) => arr.concat(this.$slots[key]), [])
+        .map(vnode => {
+          vnode.context = this._self
+          return vnode
+        })
+
+      return h(WrappedComponent, {
+        on: this.$listeners,
+        props: this.$props,
+        // 透传 scopedSlots
+        scopedSlots: this.$scopedSlots,
+        attrs: this.$attrs,
+        ...options.call(this)
+      }, slots)
     }
+  }
 }
 
+export default {
+  name: 'DebounceButton',
+  props: {
+    wait: {
+      type: Number,
+      default: 300
+    },
+    options: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  ...hoc('button', function () {
+    return {
+      on: {
+        ...this.$listeners,
+        click: debounce(this.$listeners.click, this.wait, this.options)
+      }
+    }
+  })
+}
 
 // 使用方式
+// 全局或局部注册后
 // <debounce-button :wait="250" :options="{maxWait: 1000}" @click="clickHandler">测试</debounce-button>
+
+
+
+// 导出新组件
+// export default {
+//   name: 'DebounceButton',
+//   inheritAttrs: false,
+//   props: {
+//     wait: {
+//       type: Number,
+//       default: 300
+//     },
+//     options: {
+//       type: Object,
+//       default() {
+//         return {};
+//       }
+//     }
+//   },
+//   mounted() {
+//     console.log('HOC succeed')
+//   },
+//   render(h) {
+//     const slots = Object.keys(this.$slots)
+//       .reduce((arr, key) => arr.concat(this.$slots[key]), [])
+//       .map(vnode => {
+//         vnode.context = this._self
+//         return vnode
+//       })
+//     //eg. el-button
+//     return h('Button', {
+//       on: {
+//         click: debounce(this.$listeners.click, this.wait, this.options)
+//       },
+//       props: this.$props,
+//       // 透传 scopedSlots
+//       scopedSlots: this.$scopedSlots,
+//       attrs: this.$attrs
+//     }, slots)
+//   }
+// }
