6131ad1dbed5cc9e6e2807ccb18f0ace8081ea99
yaokunpeng <y1050661817@gmail.com>
2020-11-29 18:53:49 +0800

    feat：(es6) String interpolate rely on tmpstr

--- FrontEnd-SkillPractice/JS-Practice/ES6/template-string.js
+++ FrontEnd-SkillPractice/JS-Practice/ES6/template-string.js
@@ -12,3 +12,14 @@ function fn(string, substitute) {
 const version = 'ES6';
 const params = 'second 模板 params'
 const result = fn`${version} was a major update${params}`;
+
+
+String.prototype.interpolate = function (params) {
+    const names = Object.keys(params);
+    const vals = Object.values(params);
+    console.log(names)
+    console.log(vals)
+    return new Function(...names, `return \`${this}\`;`)(...vals);
+}
+let R = '${a}原文本使用es6的模板字符串${b}'.interpolate({ a: '通过参数插值', b: '实现技巧' });
+console.log(R)
