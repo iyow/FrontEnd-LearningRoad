2879735442265982a6c0c0aacc1aa3dcfad1acc5
yaokunpeng <y1050661817@gmail.com>
2021-06-14 20:46:09 +0800

    fix：(node) http server add header and cors

--- Node/API-Practice/HTTP/server.js
+++ Node/API-Practice/HTTP/server.js
@@ -2,12 +2,24 @@ const http = require('http');
 
 // 创建一个http serve 并监听8090端口 放入异步队列中等待触发
 let server = http.createServer((request, response) => {
+    console.log(request.url)
+    // 处理跨域
+    //访问控制允许来源：所有
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    //访问控制允许报头 X-Requested-With: xhr请求
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    //访问控制允许方法
+    response.setHeader('Access-Control-Allow-Metheds', 'PUT, POST, GET, DELETE, OPTIONS');
+    //自定义头信息，表示服务端用nodejs
+    response.setHeader('X-Powered-By', 'nodejs-ykp-server');
 
 
-    response.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
     let serveradd = server.address();
+    response.writeHead(200, { "Content-Type": "text/html;charset=utf-8" });
+    // res.write(data)
+    // res.end()
     response.end(`欢迎${serveradd.address}:${serveradd.port}:${serveradd.family}访问locathost8090`)
 });
 
 
-server.listen(8090,'127.0.0.1');
+server.listen(8090, '127.0.0.1');
